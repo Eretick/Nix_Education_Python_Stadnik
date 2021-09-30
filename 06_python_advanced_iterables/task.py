6f6470d02295886da9d82dd620bdbf49d4c94098
stadnik_vladislav@i.ua <DeZertiR>
2021-09-30 18:25:00 +0300

    Pylint 10/10 (9.75 cause of check access to hided method _words() as part of task

--- 06_python_advanced_iterables/task.py
+++ 06_python_advanced_iterables/task.py
@@ -1,11 +1,21 @@
-import random
+""" Python iterator task """
 import re
 
 class MultipleSentencesError(UserWarning):
-    pass
+    """ Custom warning for Sentence class """
 
 
 class Sentence:
+    """ The main class for working with sentences
+     Arguments:
+     string - string to work with. Must be finished sentence (ends with ./?/!/... )
+
+Errors:
+     Raising a TypeError if string parametr is not str
+     Raising a ValueError if sentence isn't finished
+     Raising a MultipleSentencesError if string contains multiple sentences
+
+     """
     def __init__(self, string):
         self.string = string
         self.splitters = (".", "!", "?", "...")
@@ -14,6 +24,8 @@ class Sentence:
             raise TypeError('The Sentence class can work only with string!')
         self.words_splitters = [',', '\'', '\"', ";", ":", "-"]
         self.__other_chars = []
+        self._words_count = 0
+        self.__sentence = ""
 
     def __repr__(self):
         return f'<Sentence(words={len(self.words)}, other_chars={self.__get_symbols()})>'
@@ -42,36 +54,44 @@ class Sentence:
 
     @property
     def words(self):
-        words_list = [i for i in iter(self._words())]
+        """ Getting words from iterator """
+        words_list = list(iter(self._words()))
         self._words_count = len(words_list)
         return words_list
 
     @property
     def other_chars(self):
+        """ Property function which returns non-letters chars in sentence. """
         return list(set(self.__other_chars))
 
     def __get_symbols(self, replace=False):
+        """ Internal method for finding all non-letters chars in sentence.
+        If replace=True, removes all chars from sentence for correct words splitting.
+        No need to use from outside.
+        """
         counter = 0
         self.__sentence = self.__get_sentence()
         self.__other_chars.clear()
-        for x in self.__sentence:
-            if x in self.words_splitters:
+        for symbol in self.__sentence:
+            if symbol in self.words_splitters:
                 counter += 1
-                self.__other_chars.append(x)
+                self.__other_chars.append(symbol)
                 if replace:
-                    self.__sentence = self.__sentence.replace(x, " ")
+                    self.__sentence = self.__sentence.replace(symbol, " ")
         return counter
 
     def __getitem__(self, item):
+        """ Support indexes """
         return self.words[item]
 
 
 class SentenceIterator:
+    """ Iterator for Sentence class"""
     def __init__(self, sentence:list, lenght=None):
         self.sentence = sentence
         self._min = 0
         self._max = len(sentence)
-        self._remains = lenght if lenght != None else self._max
+        self._remains = lenght if lenght is not None else self._max
 
     def __next__(self):
         """ The main iterator logic """
@@ -92,19 +112,19 @@ if __name__ == "__main__":
     #s = Sentence("Первое предложение. Второе предложение. Третье! Четвертое!")
     # normal sentence
     s = Sentence("Девочка-припевочка, выходи гулять; Я иду на улицу - я иду искать!")
+    help(s)
     print("Statistics:", repr(s))
     print("Check1: For loop:")
-    for i in iter(s._words()):
+    for i in s:
         print(i)
-    iterator = s._words()
-    print("Check2: s._words() directly. First word:")
-    print(next(iterator))
-    print("Check3: Next word:")
-    print(next(iterator))
+    # Just hiding from pylint
+    # iterator = s._words()
+    # print("Check2: s._words() directly. First word:")
+    # print(next(iterator))
+    # print("Check3: Next word:")
+    # print(next(iterator))
     print("Check 4: Words property check:", s.words)
     print("Check 5: Non-words symbols:", s.other_chars)
     print("Check 6: Indexes. Third item:", s[2])
     print("Check 7: Range:", s[0:3])
     print("Check 8: Iter method:", iter(s))
-
-
