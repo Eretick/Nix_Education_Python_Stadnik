1ce334b2007a08c1811c85472674489e6499215f
Stadnik Vladislav <stadnik_vladislav@i.ua>
2021-10-03 01:23:16 +0300

    Completed

--- 07_python_additional_practice/task.py
+++ 07_python_additional_practice/task.py
@@ -13,13 +13,74 @@ def gen(words):
     for word in words:
         yield word
 
+class MyIterator:
+    """ Custom iterator class """
+    def __init__(self, data):
+        self.data = data
+        self.len = len(data)
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < self.len:
+            text = self.data[self.index]
+            self.index = self.index + 1
+            return text
+
+        raise StopIteration
+
+
 @work_logger
 def test_func_1():
+    """ Example for demonstrate decorator's work """
     print("Test functions working...")
+
+
+class TempList(list):
+    """ Custom context list-manager """
+    def __init__(self, data: list):
+        self.data = data
+        super().__init__()
+
+    def __enter__(self):
+        return self.data
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.data.clear()
+
 
 if __name__ == "__main__":
     print("Check1: decorator.")
     test_func_1()
+
+    generator = gen(['word1', 'word2'])
+    print("Generator", generator)
     print("Check2: generator in for loop.")
-    for i in gen(['word1', 'word2']):
+    for i in generator:
         print(i)
+
+    print("Check3: generator with 2 next():")
+    generator = gen(['word1', 'word2'])
+    print(next(generator))
+    print(next(generator))
+
+    iterator = MyIterator(["word3", "word4"])
+    print("Iterator", iterator)
+    print("Check4: iterator in for loop.")
+    for i in iterator:
+        print(i)
+
+    print("Check5: iterator with 2 next():")
+    iterator = gen(['word5', 'word6'])
+    print(next(iterator))
+    print(next(iterator))
+
+    print("Check6: Custom context manager.")
+    temp_list = TempList(["word7", "word8", "word9"])
+    print("Custom self-clear list:", type(temp_list))
+    with temp_list as t:
+        print("Looking on data while opened:")
+        print(t)
+    print("Looking on data after closing:", t)
