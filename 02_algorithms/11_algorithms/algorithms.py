e6d1578c9ad3939401bc0d3a7c4884f38a500ec1
Стадник Владислав <stadnik_vladislav@i.ua>
2021-10-18 14:56:12 +0300

    Resolved conflicts

--- 02_algorithms/11_algorithms/algorithms.py
+++ 02_algorithms/11_algorithms/algorithms.py
@@ -1,7 +1,7 @@
 """ Implementation of binary sorting algorithm  """
 
 
-def binary(data: list, search_item):
+def binary_search(data: list, search_item):
     """ Function for creating sorted list by binary search
     :list - list of sorted data
     :returns index of element
@@ -16,6 +16,13 @@ def binary(data: list, search_item):
     data_copy = data.copy()
     cur_start = 0
     cur_end = max_index
+
+    # check if item in search list range to avoid extra calculation
+    if search_item > data[max_index-1]:
+        return -1
+    elif search_item < data[0]:
+        return -1
+    # main search algorithm
     for i in range(len(data)):
         mid_index = len(data_copy)//2
         mid_item = data_copy[mid_index]
@@ -29,3 +36,45 @@ def binary(data: list, search_item):
             cur_start += mid_index
             data_copy = data[cur_start:cur_end]
     return index
+
+def quick_sort(data:list):
+    """ Implementation of quick sort algorhitm
+     :data - list of unsorted values
+     """
+    if len(data) > 0:
+        last_index = len(data)-1
+        last_item = data[len(data)-1]
+        new_data = data[:last_index]
+        data_left = []
+        data_right = []
+        for i in range(len(new_data)):
+            if new_data[i] < last_item:
+                data_left.append(new_data[i])
+            elif new_data[i] >= last_item:
+                data_right.append(new_data[i])
+
+        left_sorted = []
+        for i in range(len(data_left)-1):
+            if data_left[i+1] > data_left[i]:
+                left_sorted.append(True)
+        right_sorted = []
+        for i in range(len(data_right)-1):
+            if data_right[i+1] > data_right[i]:
+                right_sorted.append(True)
+
+        if len(left_sorted) != len(data_left):
+            data_left = quick_sort(data_left)
+        if len(right_sorted) != len(data_right):
+            data_right = quick_sort(data_right)
+        result = data_left + [last_item]+ data_right
+        return result
+
+def my_factorial(number):
+    """ Recursive factorial implementation """
+    if number < 0:
+        raise ValueError("Factorial can be only numbers > 0!")
+    elif number == 0:
+        return 1
+    elif number == 1:
+        return number
+    return number * my_factorial(number-1)
--- 02_algorithms/11_algorithms/tests.py
+++ 02_algorithms/11_algorithms/tests.py
@@ -1,24 +1,44 @@
+import math
+
 import pytest
-from algorithms import binary
+from algorithms import binary_search, quick_sort, my_factorial
 
 
-def test_sorted_binary():
+def test_binary_sorted():
     with pytest.raises(ValueError):
-        assert binary([1,3,2,6,5], 2)
+        assert binary_search([1, 3, 2, 6, 5], 2)
 
 
-def test_correct_numbers():
+def test_binary_correct_numbers():
     data = [1, 3, 4, 5, 6, 8, 9, 15, 26]
-    assert binary(data, 1) == 0, "Wrong 1st item index"
-    assert binary(data, 2) == -1, "Wrong not found index"
-    assert binary(data, -2) == -1, "Wrong not found index"
-    assert binary(data, 12) == -1, "Wrong not found index"
-    assert binary(data, 26) == 8, "Wrong 1st item index"
+    assert binary_search(data, 1) == 0, "Wrong 1st item index"
+    assert binary_search(data, 2) == -1, "Wrong not found index"
+    assert binary_search(data, -2) == -1, "Wrong not found index"
+    assert binary_search(data, 12) == -1, "Wrong not found index"
+    assert binary_search(data, 26) == 8, "Wrong 1st item index"
 
 
-def test_correct_letters():
+def test_binary_correct_letters():
     data = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    assert binary(data, "h") == 7, "Wrong 8th letter element index"
-    assert binary(data, "z") == -1, "Wrong not found letter"
-    assert binary(data, "H") == -1, "Wrong not found letter"
+    assert binary_search(data, "h") == 7, "Wrong 8th letter element index"
+    assert binary_search(data, "z") == -1, "Wrong not found letter"
+    assert binary_search(data, "H") == -1, "Wrong not found letter"
 
+
+@pytest.mark.parametrize("list", [[-1, 1, 2,  4, -2, 9, 12, 4, 42, 24], ["a", "b", "e", "z", "A"]])
+def test_quick_sort_numbers(list):
+    a = [-1, 1, 2, 3, 7, 5, 4, -2, 9, 12, 10, 15, 4, 42, 24]
+    assert quick_sort(a) == sorted(a), "Wrong sorting result!"
+
+
+@pytest.mark.parametrize("input", [1, 5, 2, 10, 0])
+def test_factorial(input):
+    from math import factorial
+    assert my_factorial(input) == math.factorial(input), "Error in factorial function. Wrong result."
+
+
+@pytest.mark.parametrize("input", [-5])
+def test_factorial_negative(input):
+    from math import factorial
+    with pytest.raises(ValueError):
+        assert my_factorial(input) == math.factorial(input), "Error in factorial function. Wrong result."
